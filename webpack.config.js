86a13aee0d9527bbe3fe8ddf4b440bb05c1861e5
Jacob Childress <jacobc@gmail.com>
2017-02-23 09:34:25 -0600

    Removed commented-out configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,6 @@ var path = require('path');
 module.exports = {
   entry: './handler.js',
   target: 'node',
-  // output: {
-  //   path: path.join(__dirname, 'dist'),
-  //   library: 'commonjs2'
-  // },
-  // externals: {
-  //   'aws-sdk': 'aws-sdk'
-  // },
   module: {
     loaders: [
       {
